a6c6afc59324ff6871b858ff425f31962dbe9208
somepanda <somesweetrain@gmail.com>
2018-10-25 15:39:17 +0100

    add general answers

--- public/javascripts/chatBot.js
+++ public/javascripts/chatBot.js
@@ -42,7 +42,7 @@
                 message_side: message_side
             });
 
-            sendResponse(text);
+            if (message_side === "left") sendResponse(text);
 
             message.draw();
             return $messages.animate({ scrollTop: $messages.prop('scrollHeight') }, 300);
@@ -67,7 +67,49 @@
 
         function sendResponse(text) {
 
-            
+            if (text.includes("How are you") || text.includes("how are you")) {
+                setTimeout(function () {
+                    return sendMessage("I'm doing fine, thank you!", "right");
+                }, 1000);
+            } else if (text.includes("Who are you") || text.includes("who are you")) {
+                setTimeout(function () {
+                    return sendMessage("I'm Cogni. I'm not a medical bot yet, but I'm planning to go to med school!", "right");
+                }, 1000);
+            } else if (text.includes("my name is") || text.includes("My name is")) {
+                setTimeout(function () {
+                    return sendMessage("Hi, I'm pleased to meet you! How are you doing?", "right");
+                }, 1000);
+            } else if (text.includes("doing good") || text.includes("doing fine")) {
+                setTimeout(function () {
+                    const texts = ["Well I hope that your life will improve even more!", "I'm really glad to hear that you are doing good!", "If you are doing good, I am doing good!"];
+                    const chosen_message = texts[Math.floor(Math.random()*texts.length)];
+                    return sendMessage(chosen_message, "right");
+                }, 1000);
+            } else if (text.includes("doing bad") || text.includes("not good") || text.includes("not so good") || text.includes("fine...") || text.includes("awful")) {
+                setTimeout(function () {
+                    return sendMessage("Well, that's not good at all? What's wrong?", "right");
+                }, 1000);
+            } else if (text.includes("headache") || text.includes("Headache") || text.includes("head ache")) {
+                setTimeout(function () {
+                    return sendMessage("Hmm...Maybe you're dehydrated. I would advise to mix a bit of salt and sugar to lemon water. It usually helps me!", "right");
+                }, 1000);
+            } else if (text.includes("ache")) {
+                setTimeout(function () {
+                    return sendMessage("Well, then I would advise to consult your doctor. Maybe you should take a day off?", "right");
+                }, 1000);
+            } else if (text.includes("died")) {
+                setTimeout(function () {
+                    return sendMessage("That really is awful", "right");
+                }, 1000);
+            } else if (text.includes("kill myself") || text.includes("hurt myself")) {
+                setTimeout(function () {
+                    return sendMessage("That is awful! I hope that you've consulted a professional!", "right");
+                }, 1000);
+            } else {
+                setTimeout(function () {
+                    return sendMessage("Sorry, some sentences are hard to understand for me! I'm just learning!", "right");
+                }, 1000);
+            }
 
         }
     });
